c7d1aa3acada850f11e9641097b75e112dd82a8a
Fariz Achmad Alfian <farizalfian234@gmail.com>
2026-06-06 03:48:10 +0000

    feat(phase-7A): Angular SSR migration + production env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://167.99.68.220:8080',
+  apiUrl: 'https://fx-brief.com/api',
   googleOAuthClientId: '627858842831-2b76sbcgvfbvfsag3h4m4mjkum2hu2em.apps.googleusercontent.com'
 };
